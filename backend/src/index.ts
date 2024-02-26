b38036e04999a5fd4bb1dd1c0585245add4bf53c
samrat-p <mitra.stark.68@gmail.com>
2024-02-27 02:31:05 +0530

    added bunch of dependencies for frontend make them app compatilble

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
 app.use(express.json());
@@ -8,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express endpoint" });
+  res.json({ message: " well i did the testing thing correctly" });
 });
 
 app.listen(7000, () => {
