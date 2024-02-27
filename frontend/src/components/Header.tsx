03e80539eb071ec2306c32a8e826b32877165fa5
samrat-p <mitra.stark.68@gmail.com>
2024-02-28 00:29:07 +0530

    fixed header section

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 const Header = () =>{
     return(
         <div className="bg-blue-800 py-6 ">
-            <div className="container mx-auto flex justify-content-between">
+            <div className="container mx-auto flex justify-between">
                 <span className="text-3xl text-white font-bold tracking-tight">
                     <Link to = "/">Mernholidays.com</Link>
                 </span>
