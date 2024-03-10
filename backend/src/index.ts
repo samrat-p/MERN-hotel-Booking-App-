8a2ab61554ee3e601f5bd295395b8db9d3c5dc95
samrat-p <mitra.stark.68@gmail.com>
2024-03-11 01:17:30 +0530

    added auth.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,22 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
-//const db = mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function() {
-//console.log("MongoDB connected successfully");
-//}); //mongodb connection status 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+console.log("MongoDB connected successfully");
+}); //mongodb connection status 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 
 //app.get("/api/test", async (req: Request, res: Response) => {
