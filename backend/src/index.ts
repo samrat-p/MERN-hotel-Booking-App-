f05c427fe4cba12af96219b2755fa027f1a69b66
samrat-p <mitra.stark.68@gmail.com>
2024-03-10 19:34:37 +0530

    added express validator (validate of required fields)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,8 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 //const db = mongoose.connection;
 //db.on('error', console.error.bind(console, 'connection error:'));
 //db.once('open', function() {
-  //console.log("MongoDB connected successfully");
-//}); mongodb connection status 
+//console.log("MongoDB connected successfully");
+//}); //mongodb connection status 
 
 const app = express();
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(cors());
 app.use("/api/users", userRoutes)
 
 //app.get("/api/test", async (req: Request, res: Response) => {
-  //res.json({ message: " well i did the testing thing correctly" });
+//res.json({ message: " well i did the testing thing correctly" });
 //}); .. testing the api (if it is running correctly or not)
 
 app.listen(7000, () => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,38 +1,54 @@
 import express, { Request, Response, Router } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  try {
-    let user = await User.findOne({
-      email: req.body.email,
-    });
-    if (user) {
-      return res.status(400).json({ message: "user already exist !!!" });
+router.post(
+  "/register",
+  [
+    check("firstname", "First Name is required").isString(),
+    check("lastname", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
     }
-    user = new User(req.body);
-    await user.save();
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
+      if (user) {
+        return res.status(400).json({ message: "user already exist !!!" });
+      }
+      user = new User(req.body);
+      await user.save();
 
-    const token = jwt.sign(
-      { userId: user.id },
-      process.env.JWT_SECRET_KEY as string,
-      { expiresIn: "1d" }
-    );
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "1d" }
+      );
 
-    res.cookie("auth_token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 846000000,
-    });
-    return res.sendStatus(200);
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .send({ message: "something is gone very wrong ... !!!! moye moye" });
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 846000000,
+      });
+      return res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .send({ message: "something is gone very wrong ... !!!! moye moye" });
+    }
   }
-});
+);
 
 export default router;
