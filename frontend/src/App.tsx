3a71601cf6751fab24b2c578c9b794af9f9460c9
samrat-p <mitra.stark.68@gmail.com>
2024-03-11 03:37:00 +0530

    added user registration frontend with all required fields validation needed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,42 @@ import {
   Route,
   Routes,
   Navigate,
-}
-from "react-router-dom";
-import Layout from "./layouts/Layout"
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
-const App = ()=>{
-  return(
+const App = () => {
+  return (
     <Router>
       <Routes>
-        <Route path = "/" element ={<Layout>
-          <p>Home Page</p>
-        </Layout>}/>
-        <Route path="/search" element={<Layout>
-          <p>Search Page</p>
-        </Layout>}/>
-        <Route path="*" element={<Navigate to="/" />}></Route>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,112 @@
+import { useForm } from "react-hook-form";
+
+type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmpassword: string;
+}; // the register form Schema...
+const Register = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>(); // uses react register,watch nd form0-submit using react-hooks, the fromstate takes an object "errors" which will handel all the errors while validate and submitting the form
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  }); // this function handels the submitfrom nd return if theres any error when submitting a form
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font bold"> Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "This field is required" })}
+          ></input>
+          {errors.firstName && ( //we are checking the firstname errors object, if this is true we will print out the error message in red, && that means if the left is true then execute
+            <span className="text-red-500 text-bold">
+              {errors.firstName.message}
+            </span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "This field is required" })}
+          ></input>
+          {errors.lastName && ( //we are checking the lastname errors object, if this is true we will print out the error message in red, && that means if the left is true then execute
+            <span className="text-red-500 text-bold">
+              {errors.lastName.message}
+            </span>
+          )}
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        ></input>
+        {errors.email && ( //we are checking the email fields errors object, if this is true we will print out the error message in red, && that means if the left is true then execute
+          <span className="text-red-500 text-bold">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "The password must be more than 6 characters",
+            },
+          })}
+        ></input>
+        {errors.password && ( //we are checking the password fields errors object, if this is true we will print out the error message in red, && that means if the left is true then execute
+          <span className="text-red-500 text-bold">
+            {errors.password.message}
+          </span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmpassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Your password do not match";
+              }
+            },
+          })}
+        ></input>
+        {errors.confirmpassword && ( //we are checking the currentpassword errors object, if this is true we will print out the error message in red, && that means if the left is true then execute
+          <span className="text-red-500 text-bold">
+            {errors.confirmpassword.message}
+          </span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 flex items-center space-x-2"
+        >
+          Create Account
+        </button>
+      </span>
+    </form>
+  );
+};
+
+export default Register;
