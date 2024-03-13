9d245cf31d48d076fcee8537ce91575d6d452db8
samrat-p <mitra.stark.68@gmail.com>
2024-03-14 02:23:24 +0530

    small changes to be made and major bug in fetch request when submitting form data

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -3,10 +3,10 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const register = async (formData: RegisterFormData) => {
     const response = await fetch(
-        `${API_BASE_URL}/api/users / register`, {
-        method: "POST",
+        `${API_BASE_URL}/api/users/register`, {
+        method: 'POST',
         headers: {
-            "Content-Type" : "application/json",
+            'Content-Type' : 'application/json',
         },
         body: JSON.stringify(formData),
     }
