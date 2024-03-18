5cd0f3b28f49cde913e87e68c1f9cc920f709c21
samrat-p <mitra.stark.68@gmail.com>
2024-03-19 04:07:24 +0530

    deployment bulid for frontend and backend added static frontend to backend so the localhost:7000 would run backend and frontend, doesnt have to start frontend again
    tomorrow will add github deployment !!!
    Good Night !

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+import path, { dirname } from "path";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 //.then(() => 
@@ -22,7 +23,7 @@ app.use(
     credentials: true,
   })
 );
-
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
