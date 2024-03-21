3b930e9bd263356f104baf45f9532d8862f3ee29
samrat-p <mitra.stark.68@gmail.com>
2024-03-22 04:04:23 +0530

    fixed index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,16 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
-import path, { dirname } from "path";
+import path from "path";
 import {v2 as cloudinary} from 'cloudinary';
 import myHotelRoutes from "./routes/my-hotels"
 
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
   api_key:process.env.CLOUDINARY_API_KEY,
   api_secret:process.env.CLOUDINARY_API_SECRET,
 })
@@ -39,6 +39,10 @@ app.use("/api/my-hotels", myHotelRoutes)
 //app.get("/api/test", async (req: Request, res: Response) => {
 //res.json({ message: " well i did the testing thing correctly" });
 //}); .. testing the api (if it is running correctly or not)
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(7000, () => {
   console.log("server is running on localhost:7000");
