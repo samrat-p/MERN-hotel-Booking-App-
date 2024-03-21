48574aa755c5edbacd4843b6ea40c42a0da2a62f
samrat-p <mitra.stark.68@gmail.com>
2024-03-22 02:50:35 +0530

    fixed buged on showing error starRating property on HotelFromData

--- frontend/src/forms/ManageHotelForm/DetalisSection.tsx
+++ frontend/src/forms/ManageHotelForm/DetalisSection.tsx
@@ -1,12 +1,15 @@
-import { useFormContext } from "react-hook-form"
+import { useFormContext } from "react-hook-form";
 import { HotelFormData } from "./ManageHotelForm";
 
-const DetalisSection = () =>{
-    const {register, formState: {errors},} = useFormContext<HotelFormData>();
-    return(
-        <div className="flex flex-col gap-4">
-            <h1 className="text 3xl font-bold mb-3">Add Hotel</h1>
-            <label className="text-gray-700 text-sm font-bold flex-1">
+const DetalisSection = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-3xl font-bold mb-3">Add Hotel</h1>
+      <label className="text-gray-700 text-sm font-bold flex-1">
         Name
         <input
           type="text"
@@ -17,10 +20,71 @@ const DetalisSection = () =>{
           <span className="text-red-500 text-bold">{errors.name.message}</span>
         )}
       </label>
-
-
-        </div>
-    )
-}
+      <div className="flex gap-4">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          City
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("city", { required: "This field is required" })}
+          ></input>
+          {errors.city && (
+            <span className="text-red-500 text-bold">
+              {errors.city.message}
+            </span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Country
+          <input
+            type="text"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("country", { required: "This field is required" })}
+          ></input>
+          {errors.country && (
+            <span className="text-red-500 text-bold">
+              {errors.country.message}
+            </span>
+          )}
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Description
+        <textarea
+          rows={10}
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("description", { required: "This field is required" })}
+        ></textarea>
+        {errors.description && (
+          <span className="text-red-500 text-bold">
+            {errors.description.message}
+          </span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+        Price Per Night
+        <input
+          type="number"
+          min={1}
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("pricePerNight", { required: "This field is required" })}
+        ></input>
+        {errors.pricePerNight && (
+          <span className="text-red-500 text-bold">
+            {errors.pricePerNight.message}
+          </span>
+        )}
+      </label>
+      <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+        Star Rating
+        {errors.starRating && (
+          <span className="text-red-500 text-bold">
+            {errors.starRating.message}
+          </span>
+        )}
+      </label>
+    </div>
+  );
+};
 
 export default DetalisSection;
