030343297d59b0c7b622957dd0437bc6e141cb4f
samrat-p <mitra.stark.68@gmail.com>
2024-03-22 02:11:51 +0530

    imported the hotel-detali-section element

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,21 +1,27 @@
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
+import DetalisSection from "./DetalisSection";
 
 export type HotelFormData = {
-    name:string;
-    city: string;
-    country: string;
-    description: string;
-    type: string;
-    pricePerNight: number;
-    facilities: string[];
-    imageFiles: FileList; //filelist rathar than array of strings
-    adultCount: number;
-    childCount: number;
-
-}
-const ManageHotelForm = () =>{
-    const formMethods = useForm<HotelFormData>();
-    return(<form></form>)
-}
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  facilities: string[];
+  imageFiles: FileList; //filelist rathar than array of strings
+  adultCount: number;
+  childCount: number;
+};
+const ManageHotelForm = () => {
+  const formMethods = useForm<HotelFormData>();
+  return (
+    <FormProvider {...formMethods}>
+      <form>
+        <DetalisSection/>
+      </form>
+    </FormProvider>
+  );
+};
 
 export default ManageHotelForm;
