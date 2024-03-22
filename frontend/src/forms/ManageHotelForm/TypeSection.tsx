38ac0ee56dd6a1a3eb9f6082ef428b75f2d1a49d
samrat-p <mitra.stark.68@gmail.com>
2024-03-23 03:35:10 +0530

    fixed a flexbox gap

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -11,10 +11,10 @@ const TypeSection = () => {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3"> Type </h2>
-      <div className="grid grid-cols-5 gap-2">
+      <div className="grid grid-cols-5 gap-3">
         {hotelTypes.map((type) => (
           <label
-            className={
+            className= {
               typeWatch === type
                 ? "cursor-poiter bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
                 : "cursor-poiter bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold"
