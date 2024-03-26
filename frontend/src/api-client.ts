39e633ac7cb8ee2a3bc853fc3002021e4adf3032
samrat-p <mitra.stark.68@gmail.com>
2024-03-27 00:50:14 +0530

    fixed hotel saved issue

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -37,7 +37,7 @@ export const signIn = async (formData: SignInFormData) =>{
 export const validateToken = async () => {
   const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
     credentials: "include"
-  })
+  });
   if(!response.ok){
     throw new Error("Token invalid-yes");
     }
