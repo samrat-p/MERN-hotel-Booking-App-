b58e3c1b9af2c28748fc26485c15d73d007c9a8c
samrat-p <mitra.stark.68@gmail.com>
2024-04-01 02:19:41 +0530

    ..//

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -4,6 +4,8 @@ import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
 import ImageSection from "./ImagesSection";
+import { HotelType } from "../../../../backend/src/shared/types";
+import { useEffect } from "react";
 
 export type HotelFormData = {
   name: string;
@@ -15,59 +17,66 @@ export type HotelFormData = {
   facilities: string[];
   imageFiles: FileList; //filelist rathar than array of strings
   adultCount: number;
-  starRating:number;
+  starRating: number;
   childCount: number;
   currency: string[]; //add currency with pricepermonth
 };
 
 type Props = {
-  onSave: (hotelFormData: FormData)=> void
+  hotel: HotelType;
+  onSave: (hotelFormData: FormData) => void;
   isLoading: boolean;
-}
-const ManageHotelForm = ({onSave, isLoading}: Props) => {
+};
+const ManageHotelForm = ({ onSave, isLoading, hotel }: Props) => {
   const formMethods = useForm<HotelFormData>();
-  const {handleSubmit} = formMethods;
-  const onSubmit = handleSubmit((formDataJson:HotelFormData)=>{
+  const { handleSubmit, reset } = formMethods;
+
+  useEffect(() => {
+    reset(hotel);
+  }, [hotel, reset]); //reset the form anytimes when a hotel changes
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
     //create new formdata object & call our api
     const formData = new FormData();
-    formData.append("name",formDataJson.name)
-    formData.append("city",formDataJson.city)
-    formData.append("country",formDataJson.country)
-    formData.append("description",formDataJson.description)
-    formData.append("type",formDataJson.type)
-    formData.append("pricePerNight",formDataJson.pricePerNight.toString())
-    formData.append("adultCount",formDataJson.adultCount.toString())
-    formData.append("childCount",formDataJson.childCount.toString())
-    formData.append("starRating",formDataJson.starRating.toString())
+    formData.append("name", formDataJson.name);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
+    formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+    formData.append("adultCount", formDataJson.adultCount.toString());
+    formData.append("childCount", formDataJson.childCount.toString());
+    formData.append("starRating", formDataJson.starRating.toString());
 
-    formDataJson.facilities.forEach((facility,index)=>{
-      formData.append(`facilities[${index}]`, facility)
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
     }); //u can choose any of the facilities, whats y we need to write a function, where foreach will iterate through every indexes of facilities and append the selected indexes in Json format
     //formDataJson.currency.forEach((currency,index)=>{
-      //formData.append(`currency[${index}]`, currency)
+    //formData.append(`currency[${index}]`, currency)
     //})
 
-    Array.from(formDataJson.imageFiles).forEach((imageFile)=>{
-        formData.append(`imageFiles`, imageFile)
-    }) //we have multar for take the indexs of these images // dont need to write a callback function for this
-onSave(formData)
-console.log(formData);
-
-
-  })
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    }); //we have multar for take the indexs of these images // dont need to write a callback function for this
+    onSave(formData);
+    console.log(formData);
+  });
   return (
     <FormProvider {...formMethods}>
       <form className="flex flex-col gap-10" onSubmit={onSubmit}>
-        <DetalisSection/>
-        <TypeSection/>
-        <FacilitiesSection/>
-        <GuestsSection/>
-        <ImageSection/>
+        <DetalisSection />
+        <TypeSection />
+        <FacilitiesSection />
+        <GuestsSection />
+        <ImageSection />
         <span className="flex justify-end">
-          <button 
-          disabled={isLoading}
-          type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled: bg-grey-500">
-            {isLoading? "Saving...": "Save"}</button>
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled: bg-grey-500"
+          >
+            {isLoading ? "Saving..." : "Save"}
+          </button>
         </span>
       </form>
     </FormProvider>
