36d10adea090a6917405f592e71290f79d9f14f1
samrat-p <mitra.stark.68@gmail.com>
2024-03-31 19:20:47 +0530

    added page for showing added hotels, with showing hotel properties and users can add hotels

---
+++ frontend/src/pages/MyHotels.tsx
@@ -0,0 +1,66 @@
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import * as apiClient from "../api-client";
+import { BsBuildingCheck, BsMap } from "react-icons/bs";
+import { BiMoney, BiSolidHotel, BiSolidStar } from "react-icons/bi";
+
+const MyHotels = () => {
+  const { data: hotelData } = useQuery("fetchHotels", apiClient.fetchMyHotels, {
+    onError: () => {},
+  });
+
+  if (!hotelData) {
+    return <span> No Hotels Found</span>;
+  }
+  return (
+    <div className="space-y-5">
+      <span className="flex justify-between">
+        <h1 className="text-3xl font-bold">My Hotels</h1>
+        <Link
+          to="/add-hotel"
+          className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
+        >
+          Add Hotels
+        </Link>
+      </span>
+      <div className="grid grid-cols-1 gap-8">
+        {hotelData.map((hotel) => (
+          <div className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5">
+            <h2 className="text=3xl font-bold">{hotel.name}</h2>
+            <div className="whitespace-pre-line">{hotel.description}</div>
+            <div className="grid grid-cols-5 gap-2">
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BsMap className="mr-2" />
+                {hotel.city},{hotel.country}
+              </div>
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BsBuildingCheck className="mr-2" />
+                {hotel.type}
+              </div>
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BiMoney className="mr-2" />${hotel.pricePerNight} Per Night
+              </div>
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BiSolidHotel className="mr-2" />
+                {hotel.adultCount} Adults, {hotel.childCount} Childs
+              </div>
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BiSolidStar className="mr-2" />
+                {hotel.starRating} Star Rating
+              </div>
+            </div>
+            <span className="flex justify-end">
+              <Link
+                to={`/edit-hotel/${hotel._id}`}
+                className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
+              >
+                View Detalis
+              </Link>
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default MyHotels;
