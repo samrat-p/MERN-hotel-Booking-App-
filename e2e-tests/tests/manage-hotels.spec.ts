37d16a79a5ae4b900fe70a6327e0db92e074d085
samrat-p <mitra.stark.68@gmail.com>
2024-03-31 20:07:00 +0530

    write test for "should display hotels"

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 const UI_URL = "http://localhost:5173/";
-import path from "path"
+import path from "path";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(UI_URL);
@@ -12,28 +12,43 @@ test.beforeEach(async ({ page }) => {
   await expect(page.getByText("Sign in Successful!")).toBeVisible();
 });
 
-test("should allow to user to add hotels", async ({page}) => {
-    await page.goto(`${UI_URL}add-hotel`);
+test("should allow to user to add hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}add-hotel`);
 
-    await expect (page.getByRole("heading",{name: "Add Hotel"})).toBeVisible();
-    await page.locator("[name=name]").fill("Mitra Executive lounge");
-    await page.locator("[name=city]").fill("Kolkata");
-    await page.locator("[name=country]").fill("India");
-    await page.locator("[name=description]").fill("E2E Testing");
-    await page.locator("[name= pricePerNight]").fill("4500");
-    await page.selectOption("[name=starRating]", "4");
-    await page.getByText("Luxury").click();
-    await page.getByLabel("Parking").check();
-    await page.getByLabel("Spa").check();
-    await page.locator('[name="adultCount"]').fill("2");
-    await page.locator('[name="childCount"]').fill("4");
+  await expect(page.getByRole("heading", { name: "Add Hotel" })).toBeVisible();
+  await page.locator("[name=name]").fill("Mitra Executive lounge");
+  await page.locator("[name=city]").fill("Kolkata");
+  await page.locator("[name=country]").fill("India");
+  await page.locator("[name=description]").fill("E2E Testing");
+  await page.locator("[name= pricePerNight]").fill("4500");
+  await page.selectOption("[name=starRating]", "4");
+  await page.getByText("Luxury").click();
+  await page.getByLabel("Parking").check();
+  await page.getByLabel("Spa").check();
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("4");
 
-    await page.setInputFiles('[name="imageFiles"]',[
-        path.join(__dirname, "files", "hotel1.jpg"),
-        path.join(__dirname, "files", "hotel2.jpg"),
-    ]);
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "hotel1.jpg"),
+    path.join(__dirname, "files", "hotel2.jpg"),
+  ]);
 
-    await page.getByRole("button", {name:"Save"}).click()
-    await expect(page.getByText("Hotel Saved")).toBeVisible()
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved")).toBeVisible();
+});
 
-})
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  //test begins
+  await expect(page.getByRole("heading", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByText("Mitra Executive lounge")).toBeVisible();
+  await expect(page.getByText("E2E Testing")).toBeVisible();
+  await expect(page.getByText("Kolkata,India")).toBeVisible();
+  await expect(page.getByText("Luxury")).toBeVisible();
+  await expect(page.getByText("$4500 Per Night")).toBeVisible();
+  await expect(page.getByText("2 Adults, 4 Childs")).toBeVisible();
+  await expect(page.getByText("4 Star Rating")).toBeVisible();
+  await expect(page.getByRole("link", { name: "View Detalis" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});
