fc751c6c7fa5f9860cf828ea5ca5d280607f9f00
samrat-p <mitra.stark.68@gmail.com>
2024-04-01 02:19:20 +0530

    added the edit-hotel page element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,12 @@ import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
-//import { useAppContext } from "./contexts/AppContext";
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
-  //const {isLoggedIn} = useAppContext()
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -41,22 +42,6 @@ const App = () => {
           }
         />
         <Route
-          path="/add-hotel"
-          element={
-            <Layout>
-              <AddHotel />
-            </Layout>
-          }
-        />
-        <Route
-          path="/my-hotels"
-          element={
-            <Layout>
-              <MyHotels />
-            </Layout>
-          }
-        />
-        <Route
           path="/sign-in"
           element={
             <Layout>
@@ -64,6 +49,43 @@ const App = () => {
             </Layout>
           }
         />
+
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel/>
+                </Layout>
+              }
+            />
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
