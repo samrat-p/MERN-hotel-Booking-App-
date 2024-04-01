8d49fd69d3343a85d6fc3a6f5e3e64054566b5af
samrat-p <mitra.stark.68@gmail.com>
2024-04-02 02:46:05 +0530

    still bugs on the code

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,26 +1,25 @@
-import { useMutation } from "react-query"
-import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm"
-import * as apiClient from "../api-client";
+import { useMutation } from "react-query";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
 
 const AddHotel = () => {
-    const {showToast} = useAppContext()
-    const{mutate,isLoading} = useMutation(apiClient.addMyHotel, {
-        onSuccess: () =>{
-            showToast({message:"Hotel Saved", type: "SUCCESS"})
-        },
-    onError: () =>{
-        showToast({message:"Error Saving Hotel", type: "ERROR"})
-    }
-    });
+  const { showToast } = useAppContext();
 
-    const handelSave = (hotelFormData: FormData)=>{
-        mutate(hotelFormData)
-    }
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
 
-    
-    return(<ManageHotelForm onSave={handelSave} isLoading = {isLoading}/>)
-}
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
 
+  return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
+};
 
 export default AddHotel;
