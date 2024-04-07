202feff6bbb810f896b1bfbe6804519f24e9083f
samrat-p <mitra.stark.68@gmail.com>
2024-04-08 02:47:25 +0530

    major buged fixed, where in edithotel page, existing hotel data doesnt showup on the form but does show up in debugging console's specifc network preview header .... #3 issue closed

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -2,23 +2,34 @@ import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
   const { hotelId } = useParams();
-  const { data: hotel } = useQuery("fetchMyHotelById", () => {
-    apiClient.fetchMyHotelById(hotelId || ''),
+  const { showToast } = useAppContext();
+
+  const { data: hotel } = useQuery(
+    "fetchMyHotelById", 
+    () => apiClient.fetchMyHotelById(hotelId || ""),
       {
         enabled: !!hotelId, //execute only when has a valid hotelid, !! defines as check for a trophy value
       }
+  );
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ message: "hotel Saved", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "error saving hotel", type: "ERROR" });
+    },
   });
-  const {mutate, isLoading} = useMutation(apiClient.updateMyHotelById, {
-    onSuccess: ()=>{},
-    onError : ()=>{}
-  })
 
   const handelSave = (hotelFormData: FormData) => {
-    mutate(hotelFormData) //functuon for handelsave button
-  }
-  return <ManageHotelForm hotel = {hotel} onSave={handelSave} isLoading={isLoading}/>
+    mutate(hotelFormData); //functuon for handelsave button
+  };
+  
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handelSave} isLoading={isLoading} />
+  );
 };
- export default EditHotel;
+export default EditHotel;
