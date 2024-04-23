2d995ba0db15a2cd3dfbd6c0120a40c27469bd0f
samrat-p <mitra.stark.68@gmail.com>
2024-04-24 01:27:51 +0530

    added starrating in search page, with using of useState hooks, so its remembers the previous selected star by user and update the hotel by new given stars

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -4,10 +4,12 @@ import * as apiClient from "../api-client";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard";
 import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
 
 const Search = () => {
   const search = useSearchContext();
   const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([])
   const searchParams = {
     destination: search.destination,
     checkIn: search.checkIn.toISOString(),
@@ -15,10 +17,22 @@ const Search = () => {
     adultCount: search.adultCount.toString(),
     childCount: search.childCount.toString(),
     page: page.toString(),
+    stars: selectedStars,
   };
   const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
     apiClient.searchHotels(searchParams)
   );
+
+  const handelStarChange= (event: React.ChangeEvent<HTMLInputElement>)=> {
+    const starRating = event.target.value;
+
+    setSelectedStars((prevStars)=>
+      event.target.checked
+    ? [...prevStars, starRating]
+    : prevStars.filter((star)=> star !== starRating)
+
+    )
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
@@ -27,7 +41,7 @@ const Search = () => {
           <h3 className="text-lg font-semibol border-b border-slate-300 pb-5">
             Filter by:
           </h3>
-          {/*todo filters */}
+          <StarRatingFilter selectedStars={selectedStars} onChange={handelStarChange} />
         </div>
       </div>
       <div className="flex flex-col gap-5">
