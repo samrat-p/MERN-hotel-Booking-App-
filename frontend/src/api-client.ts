26e2215f31a5a0395691b74ce95c240a27e12ff2
samrat-p <mitra.stark.68@gmail.com>
2024-04-26 02:11:28 +0530

    updated fetchdata by hotel_id

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -145,3 +145,11 @@ export const searchHotels = async (
   }
   return response.json();
 };
+
+export const fetchHotelById = async(hotelId:string) => {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`)
+    if(!response.ok){
+      throw new Error("Error Fetching hotels")
+    }
+      return response.json()
+  }
