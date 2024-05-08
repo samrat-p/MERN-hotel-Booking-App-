91cfa88ba58b91c1a715bd666edd488f407886e4
samrat-p <mitra.stark.68@gmail.com>
2024-05-08 21:53:18 +0530

    added endpoint for user confirmation book page

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,19 +1,36 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
+router.get("/me", verifyToken, async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 router.post(
   "/register",
   [
     check("firstName", "First Name is required").isString(),
     check("lastName", "Last Name is required").isString(),
     check("email", "Email is required").isEmail(),
-    check("password", "Password is more than 6 character is required").isLength({
-      min: 6,
-    }),
+    check("password", "Password is more than 6 character is required").isLength(
+      {
+        min: 6,
+      }
+    ),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -40,8 +57,8 @@ router.post(
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
-      });  //jwt expiry 
-      return res.status(200).send({message:"User registered OK"});
+      }); //jwt expiry
+      return res.status(200).send({ message: "User registered OK" });
     } catch (error) {
       console.log(error);
       res
