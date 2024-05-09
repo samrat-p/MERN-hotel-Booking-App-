77166ccaef4077cb9ce5fa85785c84f4749d657b
samrat-p <mitra.stark.68@gmail.com>
2024-05-10 02:28:37 +0530

    added nessecary fucntions for bookingdetailsummary

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -3,12 +3,22 @@ import * as apiClient from "../api-client";
 import BookingForm from "../forms/BookingForm/BookingForm";
 import { useSearchContext } from "../contexts/SearchContext";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import BookingDetailSummary from "../components/BookingDetailSummary";
 
 const Booking = () => {
   const search = useSearchContext();
   const { hotelId } = useParams();
-  const [numberOfNights, setNumberOfNights] = useState<number>(0)
+  const [numberOfNights, setNumberOfNights] = useState<number>(0);
+  useEffect(() => {
+    if (search.checkIn && search.checkOut) {
+      const nights =
+        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+        (1000 * 60 * 60 * 24);
+      setNumberOfNights(Math.ceil(nights));
+    }
+  }, [search.checkIn, search.checkOut]);
+
   const { data: hotel } = useQuery(
     "fetchHotelByID",
     () => apiClient.fetchHotelById(hotelId as string),
@@ -22,9 +32,19 @@ const Booking = () => {
     apiClient.fetchCurrrentUser
   );
   //console.log(currentUser?.email)
+if(!hotel) {
+  return <>Error PLS</>
+}
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
-      <div className="bg-green-200">Booking Detalis Summary</div>
+      <BookingDetailSummary
+        checkIn={search.checkIn}
+        checkOut={search.checkOut}
+        adultCount={search.adultCount}
+        childCount={search.childCount}
+        numberOfNights={numberOfNights}
+        hotel={hotel}
+      />
       {currentUser && <BookingForm currentUser={currentUser} />}
     </div>
   );
