ad1712821d51c444565d71b7bf26e15fe9cdc050
samrat-p <mitra.stark.68@gmail.com>
2024-05-09 23:32:26 +0530

    features for booking description

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,8 +1,22 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 import BookingForm from "../forms/BookingForm/BookingForm";
+import { useSearchContext } from "../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const Booking = () => {
+  const search = useSearchContext();
+  const { hotelId } = useParams();
+  const [numberOfNights, setNumberOfNights] = useState<number>(0)
+  const { data: hotel } = useQuery(
+    "fetchHotelByID",
+    () => apiClient.fetchHotelById(hotelId as string),
+    {
+      enabled: !!hotelId,
+    }
+  );
+
   const { data: currentUser } = useQuery(
     "fetchCurrentUser",
     apiClient.fetchCurrrentUser
@@ -11,7 +25,7 @@ const Booking = () => {
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
       <div className="bg-green-200">Booking Detalis Summary</div>
-      {currentUser && <BookingForm currentUser={currentUser} />} 
+      {currentUser && <BookingForm currentUser={currentUser} />}
     </div>
   );
 }; //currentUser && means it will only display if we have a currentUser
