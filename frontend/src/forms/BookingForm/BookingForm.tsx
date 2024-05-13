66e4879cce3d0c008b60a58996d9beb11c2a2508
samrat-p <mitra.stark.68@gmail.com>
2024-05-14 02:33:41 +0530

    added jsx for stripe and added stripe template for card detalis input

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -1,8 +1,13 @@
 import { useForm } from "react-hook-form";
-import { UserType } from "../../../../backend/src/shared/types";
+import {
+  UserType,
+  paymentIntentResponse,
+} from "../../../../backend/src/shared/types";
+import { CardElement } from "@stripe/react-stripe-js";
 
 type Props = {
   currentUser: UserType;
+  paymentIntent: paymentIntentResponse;
 };
 
 type BookingFormData = {
@@ -11,7 +16,7 @@ type BookingFormData = {
   email: string;
 };
 
-const BookingForm = ({ currentUser }: Props) => {
+const BookingForm = ({ currentUser, paymentIntent }: Props) => {
   const { handleSubmit, register } = useForm<BookingFormData>({
     defaultValues: {
       firstName: currentUser.firstName,
@@ -23,7 +28,7 @@ const BookingForm = ({ currentUser }: Props) => {
   return (
     <form className="grid grid-cols-1 gap-5 px-5 py-5 border rounded border-slate-300">
       <span className="text-3xl font-bold">Confirmed Your Detalis</span>
-      <div className="grid grid-cols-2 gap-6">
+      <div className="grid grid-cols-2 gap-4">
         <label className="text-gray-700 text-sm font-bold flex-1">
           First Name
           <input
@@ -44,17 +49,35 @@ const BookingForm = ({ currentUser }: Props) => {
             {...register("lastName")}
           />
         </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Email
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal "
+            type="text"
+            readOnly
+            disabled
+            {...register("email")}
+          />
+        </label>
       </div>
-      <label className="text-gray-700 text-sm font-bold flex-1">
-        Email
-        <input
-          className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal "
-          type="text"
-          readOnly
-          disabled
-          {...register("email")}
-        />
-      </label>
+      <div className="space-y-2">
+        <h2 className="text-xl font-semibold">Your Price Summary</h2>
+        <div className="bg-blue-200 rounded-md p-4">
+          <div className="font-semibold text-lg">
+            Total Cost: ${paymentIntent.totalCost.toFixed(2)}
+          </div>
+          <div className="text-xs font-semibold">
+            Included taxes and charges
+          </div>
+        </div>
+      </div>
+      <div className="space-y-2">
+        <h3 className="text-xl font-semibold">Payment Detalis</h3>
+        <CardElement
+          id="payment-element"
+          className="border rounded-md p-2 text-sm"
+        ></CardElement>
+      </div>
     </form>
   );
 };
