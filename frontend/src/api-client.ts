8863f81a9d91dd89b1aff151cb758142c9527143
samrat-p <mitra.stark.68@gmail.com>
2024-05-17 02:17:30 +0530

    multiple typo fixed in api calls

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,8 +1,18 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-import { HotelSearchResponse, HotelType, UserType, paymentIntentResponse } from "../../backend/src/shared/types";
+import { HotelSearchResponse, HotelType, UserType, PaymentIntentResponse } from "../../backend/src/shared/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+
+export const fetchCurrentUser = async (): Promise<UserType> => {
+  const response = await fetch(`${API_BASE_URL}/api/users/me`,{
+    credentials: "include"
+  })
+  if(!response.ok){
+    throw new Error("Error fetching user")
+  }
+  return response.json()
+}
 
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
@@ -112,9 +122,9 @@ export type SearchParams = {
   adultCount?: string;
   childCount?: string;
   page?: string;
-  facilities? : string[];
+  facilities?: string[];
   types?: string[];
-  stars?: string[]
+  stars?: string[];
   maxPrice?: string;
   sortOptions?: string;
 };
@@ -155,20 +165,11 @@ export const fetchHotelById = async(hotelId:string): Promise<HotelType> => {
       return response.json()
   }
 
-export const fetchCurrrentUser = async (): Promise<UserType> => {
-    const response = await fetch(`${API_BASE_URL}/api/users/me`,{
-      credentials: "include"
-    })
-    if(!response.ok){
-      throw new Error("Error fetching user")
-    }
-    return response.json()
-}
 
 export const createPaymentIntent = async (
   hotelId: string,
   numberOfNights: string
-): Promise<paymentIntentResponse> => {
+): Promise<PaymentIntentResponse> => {
   const response = await fetch(
     `${API_BASE_URL}/api/hotels/${hotelId}/bookings/payment-intent`,
     {
