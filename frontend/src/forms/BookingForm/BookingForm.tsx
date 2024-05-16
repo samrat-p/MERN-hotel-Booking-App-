5a41f6eb31c17b0de49aad0047d601d293db0e66
samrat-p <mitra.stark.68@gmail.com>
2024-05-17 02:17:52 +0530

    uses camelcase //typo errors

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import {
-  paymentIntentResponse,
+  PaymentIntentResponse,
   UserType,
 } from "../../../../backend/src/shared/types";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
@@ -13,7 +13,7 @@ import { useAppContext } from "../../contexts/AppContext";
 
 type Props = {
   currentUser: UserType;
-  paymentIntent: paymentIntentResponse;
+  paymentIntent: PaymentIntentResponse;
 };
 
 export type BookingFormData = {
