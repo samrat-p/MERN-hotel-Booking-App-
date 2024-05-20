5de8d230883a9f5bd2997e4d37eff8841472921e
samrat-p <mitra.stark.68@gmail.com>
2024-05-20 19:32:02 +0530

    ..//

--- backend/src/routes/my-bookings.ts
+++ backend/src/routes/my-bookings.ts
@@ -1,14 +1,36 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import verifyToken from "../middleware/auth";
+import Hotel from "../models/hotel";
+import { HotelType } from "../shared/types";
 
-const router = express.Router()
+const router = express.Router();
 //api/my-booking
 
-router.get("/",verifyToken, async(req: Request, res: Response) => {
-    try{
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    //finds all the hotels which have been booked
+    const hotels = await Hotel.find({
+      bookings: { $elemMatch: { userId: req.userId } },
+    });
+    //filtering out all the bookings for different users
+    const results = hotels.map((hotel) => {
+      const userBookings = hotel.bookings.filter(
+        (booking) => booking.userId === req.userId
+      );
+      //gonna update the bookings array with this new array
 
-    }catch(error){
-        console.log(error)
-        return res.status(500).json({message:"Unable to fetch bookings"})
-    }
-})
+      const hotelWithUserBookings: HotelType = {
+        ...hotel.toObject(),
+        bookings: userBookings,
+      };
+      return hotelWithUserBookings;
+    });
+
+    res.status(200).send(results);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Unable to fetch bookings" });
+  }
+});
+
+export default router
