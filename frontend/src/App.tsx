d8e3a5f13a30b3655688a0cd2d11e2a2183becc7
samrat-p <mitra.stark.68@gmail.com>
2024-05-21 01:52:31 +0530

    added mybooking pages to route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detali";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/Mybookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -92,6 +93,14 @@ const App = () => {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings/>
                 </Layout>
               }
             />
